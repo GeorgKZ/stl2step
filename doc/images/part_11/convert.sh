d571786a7b297b577b9682d8a36bca1afda1b8e1
GeorgKZ <lhanthian@gmail.com>
2025-10-05 13:43:50 +0300

    Правка документации

---
+++ doc/images/part_11/convert.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+./Simpler picture_b_01.svg part_11_picture_b_01_opt.svg
+./Simpler picture_b_02.svg part_11_picture_b_02_opt.svg
+./Simpler picture_b_03.svg part_11_picture_b_03_opt.svg
+
+./Simpler picture_d_01.svg part_11_picture_d_01_opt.svg
+./Simpler picture_d_02.svg part_11_picture_d_02_opt.svg
+./Simpler picture_d_03.svg part_11_picture_d_03_opt.svg
+./Simpler picture_d_04.svg part_11_picture_d_04_opt.svg
+./Simpler picture_d_05.svg part_11_picture_d_05_opt.svg
+./Simpler picture_d_06.svg part_11_picture_d_06_opt.svg
+./Simpler picture_d_07.svg part_11_picture_d_07_opt.svg
+./Simpler picture_d_08.svg part_11_picture_d_08_opt.svg
+./Simpler picture_d_09.svg part_11_picture_d_09_opt.svg
+./Simpler picture_d_10.svg part_11_picture_d_10_opt.svg
+./Simpler picture_d_11.svg part_11_picture_d_11_opt.svg
+./Simpler picture_d_12.svg part_11_picture_d_12_opt.svg
+./Simpler picture_d_13.svg part_11_picture_d_13_opt.svg
+./Simpler picture_d_14.svg part_11_picture_d_14_opt.svg
+./Simpler picture_d_15.svg part_11_picture_d_15_opt.svg
+./Simpler picture_d_16.svg part_11_picture_d_16_opt.svg
+./Simpler picture_d_17.svg part_11_picture_d_17_opt.svg
+./Simpler picture_d_18.svg part_11_picture_d_18_opt.svg
+./Simpler picture_d_19.svg part_11_picture_d_19_opt.svg
+./Simpler picture_d_20.svg part_11_picture_d_20_opt.svg
+./Simpler picture_d_21.svg part_11_picture_d_21_opt.svg
+./Simpler picture_d_22.svg part_11_picture_d_22_opt.svg
+./Simpler picture_d_23.svg part_11_picture_d_23_opt.svg
+./Simpler picture_d_24.svg part_11_picture_d_24_opt.svg
+./Simpler picture_d_25.svg part_11_picture_d_25_opt.svg
+./Simpler picture_d_26.svg part_11_picture_d_26_opt.svg
